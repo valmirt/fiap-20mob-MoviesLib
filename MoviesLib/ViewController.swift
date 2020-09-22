f467312a96c74f8045c28762eb49e85efc71836d
valmirt <valmirt.dev@gmail.com>
2020-09-22 19:33:57 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Valmir Junior on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
